4c926915d6270ca8398c55672b07c5e9ff87c8c0
Mr Rogers <jon@bunnymatic.com>
2015-07-20 15:26:11 -0700

    add ability to configure the output filename

--- prawnit.rb
+++ prawnit.rb
@@ -2,13 +2,16 @@
 
 
 require 'prawn'
-require 'pdf/toolkit'
+#require 'pdf/toolkit'
 
 template_file = 'some/dir/Awesome-Graphics.pdf'
-prawn_file = 'temp.pdf'
+prawn_file = ARGV[0] || 'test.pdf'
 output_file = 'output.pdf'
 
+print "Generating #{prawn_file}..."
 Prawn::Document.generate(prawn_file) do
   # Generate whatever you want here.
-  text_box "This is some new text!", :at => [100, 300]
+  text_box "Harrison Metal is the bees knees!", :at => [100, 300]
+  #text "Website: <link href='http://www.stackoverflow.com'>stackoverflow</link>", :inline_format => true
 end
+puts "done"
